a427dc926810c4149005beaf6b0afa8471c95d4c
Luiz <luiszapataconta@gmail.com>
2026-09-03 20:50:37 -0300

    metodo listarPedido e buscarID em PedidoService

---
+++ src/main/java/com/luiszapata/lanchonete/service/PedidoService.java
@@ -0,0 +1,44 @@
+package com.luiszapata.lanchonete.service;
+
+import com.luiszapata.lanchonete.model.*;
+import com.luiszapata.lanchonete.repository.*;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+public class PedidoService {
+
+    @Service
+    public class PedidoService{
+
+        private final PedidoRepository pedidoRepository;
+        private final HamburguerRepository hamburguerRepository;
+        private final BebidaRepository bebidaRepository;
+
+        public PedidoService(PedidoRepository pedidoRepository, HamburguerRepository hamburguerRepository, BebidaRepository bebidaRepository){
+            this.bebidaRepository = bebidaRepository;
+            this.pedidoRepository = pedidoRepository;
+            this.hamburguerRepository = hamburguerRepository;
+
+        }
+
+        public List<Pedido> listarPedidos(){
+            return pedidoRepository.findAll();
+
+        }
+
+        public Pedido buscarId(Long id){
+            Optional<Pedido> pedidoOptional = pedidoRepository.findById(id);
+
+            if (pedidoOptional.isPresent()){
+                return pedidoOptional.get();
+
+            } else {
+                throw new IllegalArgumentException("pedido nao encontrado ");
+
+            }
+        }
+
+    }
+}
