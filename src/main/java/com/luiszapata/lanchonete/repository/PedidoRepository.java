794ab985dac573a2eeb31f27816e3e18c0b5cd1a
Luiz <luiszapataconta@gmail.com>
2026-09-03 20:36:27 -0300

    alteracao no pedidoRepository

--- src/main/java/com/luiszapata/lanchonete/repository/PedidoRepository.java
+++ src/main/java/com/luiszapata/lanchonete/repository/PedidoRepository.java
@@ -3,6 +3,11 @@ package com.luiszapata.lanchonete.repository;
 import com.luiszapata.lanchonete.model.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+    boolean existsByCodigo(String codigo);
+    List<Pedido> findByCodigoContainingOrDescricaoContaining(String codigo, String descricao);
+
 
 }
