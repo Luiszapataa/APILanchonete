03ab36687923d010b31c417b89f694502579f29f
Luis Felipe <luiszapataconta@gmail.com>
2026-09-03 10:01:40 -0300

    criacao do metodo bucascarID e atualizar

--- src/main/java/com/luiszapata/lanchonete/service/BebidaService.java
+++ src/main/java/com/luiszapata/lanchonete/service/BebidaService.java
@@ -5,6 +5,7 @@ import com.luiszapata.lanchonete.repository.BebidaRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BebidaService {
@@ -27,6 +28,28 @@ public class BebidaService {
         } else {
             return bebidaRepository .save(bebida);
         }
+    }
+
+    public Bebida buscaId(Long id){
+        Optional<Bebida> bebidaOptional = bebidaRepository.findById(id);
+
+        if (bebidaOptional.isPresent()){
+            return bebidaOptional.get();
+        } else {
+            throw new IllegalArgumentException("bebdida nao encontrada");
+        }
+    }
+
+    public Bebida atualizar (Long id, Bebida novaBebida){
+        Bebida bebidaExistente = buscaId(id);
+
+
+        bebidaExistente.setCodigo(novaBebida.getCodigo());
+        bebidaExistente.setDescricao(novaBebida.getDescricao());
+        bebidaExistente.setPrecoUnitario(novaBebida.getPrecoUnitario());
+        bebidaExistente.setContemAcucar(novaBebida.isContemAcucar());
+
+        return bebidaRepository.save(bebidaExistente);
     }
 
 }
